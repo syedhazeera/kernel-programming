9840e48690cefcd25a00b12517ed333673e4b6f9
syedhazeera <129369062+syedhazeera@users.noreply.github.com>
2023-04-14 12:58:12 +0530

    Add files via upload

---
+++ hello1.c
@@ -0,0 +1,22 @@
+#include<linux/kernel.h>
+#include<linux/module.h>
+#include<linux/init.h>
+
+int __init hw_init(void){
+	printk(KERN_INFO"Hello World\n");
+return 0;
+}
+
+void  __exit hw_exit(void)
+{
+	printk(KERN_INFO"Exit World\n");
+}
+
+MODULE_LICENCE("GPL");
+module_init(hw_init);
+module_exit(hw_exit);
+
+
+/*output:
+pid=31463 comm="firefox" exe="/snap/firefox/2559/usr/lib/firefox/firefox" sig=0 arch=c000003e syscall=314 compat=0 ip=0x7f158324973d code=0x50000
+[ 3067.908757] Hello World*/
